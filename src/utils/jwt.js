c94ea0af806db4809286cdffe1dbb96faa06beec
Anirban <anirbansinha27@gmail.com>
2025-12-04 00:31:26 +0530

    setup cookies and jwt

---
+++ src/utils/jwt.js
@@ -0,0 +1,24 @@
+import jwt from 'jsonwebtoken';
+const JWT_SECRET = process.env.JWT_SECRET || 'secret';
+
+const JWT_EXPIRES_IN= '1d';
+
+export const jwttoken={
+    sign: (payload)=>{
+        try {
+            return jwt.sign(payload, JWT_SECRET, {expiresIn: JWT_EXPIRES_IN});
+        } catch (e) {
+            loggers.error('Failed to authenticate token',e);
+            throw new Error('Failed to authenticate token');
+        }
+    },
+
+    verify:(token)=>{
+        try {
+            return jwt.verify(token, JWT_SECRET);
+        } catch (e) {
+            loggers.error('Failed to authenticate token',e);
+            throw new Error('Failed to authenticate token');
+        }
+    }
+}
